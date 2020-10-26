1ea7dec847ef8cd04f8088940b0abb68df0d5faf
Brennan <brennanbutler01@gmail.com>
2020-10-25 22:38:24 -0700

    ready made sorting alg

--- part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
+++ part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -1,10 +1,29 @@
-
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
             int[] array = {3, 1, 5, 99, 3, 12};
-            int a =0 ;
+            sort (array);
+        System.out.println(Arrays.toString(array));
+
     }
 
+    public static void sort (int[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sort (String[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sortIntegers(ArrayList<Integer> integers) {
+        Collections.sort(integers);
+    }
+
+    public static void sortStrings(ArrayList<String> strings) {
+        Collections.sort(strings);
+    }
 }
