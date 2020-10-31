d13124b9b320bf169bf86e0a1c660def488053c0
Brennan <brennanbutler01@gmail.com>
2020-10-31 16:31:53 -0700

    finished warehouse

--- IdeaProjects/TMCProjects/mooc-java-programming-ii/part09-Part09_03.Warehousing/src/main/java/Main.java
+++ IdeaProjects/TMCProjects/mooc-java-programming-ii/part09-Part09_03.Warehousing/src/main/java/Main.java
@@ -3,12 +3,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        ProductWarehouse juice = new ProductWarehouse("Juice", 1000.0);
-        juice.addToWarehouse(1000.0);
+        ProductWarehouseWithHistory juice = new ProductWarehouseWithHistory("Juice", 1000.0, 1000.0);
         juice.takeFromWarehouse(11.3);
-        System.out.println(juice.getName()); // Juice
         juice.addToWarehouse(1.0);
-        System.out.println(juice);         // balance = 988.7, space left 11.3    }
+//System.out.println(juice.history()); // [1000.0, 988.7, 989.7]
 
+        juice.printAnalysis();
     }
 }
