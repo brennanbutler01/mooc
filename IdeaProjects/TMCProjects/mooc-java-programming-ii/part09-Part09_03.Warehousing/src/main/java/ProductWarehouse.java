e5d80183644482ae0763108ca4382cc4b6dfe110
Brennan <brennanbutler01@gmail.com>
2020-10-31 15:39:11 -0700

    pt 1 warehousing

---
+++ IdeaProjects/TMCProjects/mooc-java-programming-ii/part09-Part09_03.Warehousing/src/main/java/ProductWarehouse.java
@@ -0,0 +1,12 @@
+public class ProductWarehouse extends Warehouse{
+    private String name;
+
+    public ProductWarehouse(String name, double capacity) {
+        super(capacity);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
