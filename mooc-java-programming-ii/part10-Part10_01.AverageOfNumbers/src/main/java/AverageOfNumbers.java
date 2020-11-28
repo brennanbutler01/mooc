0b6508c0872465230b109c19a33cd3355b2e0d6b
Brennan <brennanbutler01@gmail.com>
2020-11-28 15:58:57 -0800

    average - stream

--- mooc-java-programming-ii/part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ mooc-java-programming-ii/part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,8 +5,22 @@ import java.util.Scanner;
 public class AverageOfNumbers {
 
     public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        // Write your program here
 
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            list.add(input);
+        }
+
+        double average = list.stream().
+                mapToInt(s -> Integer.valueOf(s)).
+                average().
+                getAsDouble();
+
+        System.out.println("the average of the numbers: " + average);
     }
 }
