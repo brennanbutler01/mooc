617449dc791d0d26bf3a66df9363ed5518ebf3f0
Brennan <brennanbutler01@gmail.com>
2020-11-28 19:44:27 -0800

    weighting

--- mooc-java-programming-ii/part10-Part10_08.Weighting/src/main/java/Hold.java
+++ mooc-java-programming-ii/part10-Part10_08.Weighting/src/main/java/Hold.java
@@ -21,21 +21,13 @@ public class Hold {
     }
 
     public int totalWeight() {
-        int summa = 0;
-        int indeksi = 0;
-        while (indeksi < this.suitcases.size()) {
-        summa += this.suitcases.get(indeksi).totalWeight();
-        indeksi++;
-        }
-        return summa;
+        int totalWeight = this.suitcases.stream().mapToInt(suitcase -> Integer.valueOf(suitcase.totalWeight())).reduce(0,
+                (item, sum) -> sum + item);
+        return totalWeight;
     }
 
     public void printItems() {
-        int indeksi = 0;
-        while (indeksi < this.suitcases.size()) {
-        this.suitcases.get(indeksi).printItems();
-        indeksi++;
-        }
+        this.suitcases.stream().forEach(suitcase -> suitcase.printItems());
     }
 
     @Override
--- mooc-java-programming-ii/part10-Part10_08.Weighting/src/main/java/Suitcase.java
+++ mooc-java-programming-ii/part10-Part10_08.Weighting/src/main/java/Suitcase.java
@@ -20,22 +20,11 @@ public class Suitcase {
     }
 
     public int totalWeight() {
-        int summa = 0;
-        int indeksi = 0;
-        while (indeksi < this.Items.size()) {
-        summa += this.Items.get(indeksi).getweight();
-        indeksi++;
-        }
-        return summa;
+        return this.Items.stream().mapToInt(item -> Integer.valueOf(item.getWeight())).reduce(0, (item, sum) -> item + sum);
     }
 
     public void printItems() {
-        int indeksi = 0;
-        while (indeksi < this.Items.size()) {
-        Item t = this.Items.get(indeksi);
-        System.out.println(t);
-        indeksi++;
-        }
+        this.Items.stream().forEach(item -> System.out.println(item));
     }
 
     public Item heaviestItem() {
