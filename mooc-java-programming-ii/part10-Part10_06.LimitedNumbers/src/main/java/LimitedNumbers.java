5c556900283cca175e602800ef3a25d6b0220966
Brennan <brennanbutler01@gmail.com>
2020-11-28 18:26:56 -0800

    limited numbers

--- mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,18 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> values = new ArrayList<>();
 
+        while (scanner.hasNextLine()) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input < 0) {
+                break;
+            }
+            values.add(input);
+        }
+
+        values.stream()
+                .filter(value -> value >= 1 && value <= 5 )
+                .forEach(value -> System.out.println(value));
     }
 }
