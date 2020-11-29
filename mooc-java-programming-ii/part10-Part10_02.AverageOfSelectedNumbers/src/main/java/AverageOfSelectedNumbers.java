8712f2861ff1d878c414922c95bea7196feb6e07
Brennan <brennanbutler01@gmail.com>
2020-11-28 16:08:34 -0800

    average of selected

--- mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -6,7 +6,26 @@ public class AverageOfSelectedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
+        ArrayList<String> list = new ArrayList<>();
 
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+
+            if (input.equals("end")) {
+                break;
+            }
+            list.add(input);
+        }
+
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+        String input = scanner.nextLine();
+
+        if (input.equals("p")) {
+            double average = list.stream().mapToInt(s -> Integer.valueOf(s)).filter(x -> x >= 0).average().getAsDouble();
+            System.out.println("Average of the positive numbers: " + average);
+        } else {
+            double average = list.stream().mapToInt(s -> Integer.valueOf(s)).filter(x -> x < 0).average().getAsDouble();
+            System.out.println("Average of the negative numbers: " + average);
+        }
     }
 }
